f6bda1a9b14a173729085dfb89cb1097ae7771c6
ShawnLeeLXY <lixinyangfm@163.com>
2021-06-15 00:29:24 +0800

    'JavaSE_Study_codes'

--- javaTest/src/CalendarTest.java
+++ javaTest/src/CalendarTest.java
@@ -6,7 +6,6 @@ import java.util.Calendar;
 public class CalendarTest {
     public static void main(String[] args) {
         //获取默认时区和区域设置的Calendar对象，并获取年份
-		//1
         System.out.println(Calendar.getInstance().get(Calendar.YEAR));
         return;
     }
